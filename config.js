ae3ee2492a514f37b960265b24f9b19bb889eb18
Your Name <you@example.com>
2017-08-13 10:47:19 -0400

    added mLab credential to config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/restaurants-app';
+                      'mongodb://mretfaster:Jake8Snake6@ds053944.mlab.com:53944/restaurants';
 exports.PORT = process.env.PORT || 8080;
